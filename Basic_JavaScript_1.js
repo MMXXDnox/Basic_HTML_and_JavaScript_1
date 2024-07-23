2f09c8406e8af4fcc9b1ce3c8c3bcd4d2b6f01c5
MMXXDnox <mmxxdnox@proton.me>
2024-07-23 16:24:12 +0100

    first commit

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+alert ("Hello,Welcome to David's JavaScript!");
+function My_First_Function () {
+    var String = "Congradulations, You discovered David's tressure!";
+    var result = String.fontcolor("gold");
+    document.getElementById("Gold") .innerHTML = result;
+}
